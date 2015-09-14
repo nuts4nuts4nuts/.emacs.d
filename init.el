54b15072ca2d3618888bbc32ea1666154ac4bf76
nuts4nuts4nuts <nuts4nuts4nuts@gmail.com>
2015-09-13 23:48:44 -0400

    Condense packages

--- init.el
+++ init.el
@@ -4,33 +4,32 @@
       (tool-bar-mode -1)
       (scroll-bar-mode -1))
 
-;; Unbind clipboard integration (so that I can use the VIM clipboard stuff.
-(setq x-select-enable-clipboard nil)
-
-;; IDO mode
-(ido-mode t)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-
-;; Smex on
-(global-set-key (kbd "M-x") 'smex)
-
-;; Use Marmalade repo
+;; Add repos for the package manager
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
 (package-initialize)
-(push '("melpa" . "http://melpa.milkbox.net/packages/")
-      package-archives)
 
+;;; Evil Mode things
 ;; Evil Leader on
+(defun open-init ()
+  (interactive)
+  (split-window-right)
+  (windmove-right)
+  (find-file "~/.emacs.d/init.el"))
+
 (require 'evil-leader)
 (global-evil-leader-mode)
     ;; leader configurations
     (evil-leader/set-leader "<SPC>")
     (evil-leader/set-key
-      "f" 'find-file
-      "b" 'switch-to-buffer
+      "f" 'helm-find-files
+      "b" 'helm-buffers-list
       "k" 'kill-buffer
-      "d" 'ido-dired)
+      "d" 'dired
+      "in" 'open-init)
 
 ;; Evil Mode on
 (require 'evil)
@@ -40,6 +39,7 @@
 (require 'evil-escape)
 (evil-escape-mode t)
 (setq-default evil-escape-key-sequence "kj")
+(setq-default evil-escape-delay 0.2)
 
 ;; Scroll up and down
 (define-key evil-normal-state-map (kbd "C-k") (lambda ()
@@ -48,6 +48,19 @@
 (define-key evil-normal-state-map (kbd "C-j") (lambda ()
 			(interactive)
 			(evil-scroll-down nil)))
+;;; Evil Mode things end
+
+;; Unbind clipboard integration (so that I can use the VIM clipboard stuff
+(setq x-select-enable-clipboard nil)
+
+;; Powerline makes the info bar at the bottom real pretty
+(require 'powerline)
+(powerline-evil-center-color-theme)
+
+;; Helm is the craziest thing ever
+(require 'helm-config)
+(helm-mode t)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 ;; Zenburn theme
 (custom-set-variables
@@ -65,10 +78,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; Make prompts nicer
-(setq confirm-nonexistent-file-or-buffer nil)
-(setq ido-confirm-create-new-buffer 'always)
 
 ;; Display tooltips in the echo area
 (tooltip-mode -1)
@@ -88,22 +97,8 @@
 (global-set-key (kbd "M-l") 'windmove-right)
 (global-set-key (kbd "<C-tab>") 'other-window)
 
-;; Don't prompt for nonexistent buffer
-(setq ido-create-new-buffer 'always)
-
 ;; auto-complete-mode
-(global-auto-complete-mode 1)
-
-;; Open my init file with <Leader>in
-(defun open-init ()
-  (interactive)
-  (split-window-right)
-  (windmove-right)
-  (find-file "~/.emacs.d/init.el"))
-(evil-leader/set-key "in" 'open-init)
-
-;; Use company mode
-(add-hook 'after-init-hook 'global-company-mode)
+(global-auto-complete-mode t)
 
 ;; TESTING OMNISHARP STUFF
 (require 'omnisharp)
