d95198f89ffa600ef5c5c8d3c4a63bb7960f3923
David Johnston <dvdjoh@gmail.com>
2023-12-13 20:23:23 -0500

    Formatting and remove arielle-lang agenda addition

--- noogle.el
+++ noogle.el
@@ -4,6 +4,3 @@
   (setq gptel-api-key (lambda () (getenv "OPENAIKEY"))))
 
 (define-key dkj-keys (kbd "C-<return>") #'gptel-send)
-
-(setq org-agenda-files
-      (cons "~/Documents/12lang/arielle-lang" org-agenda-files))
