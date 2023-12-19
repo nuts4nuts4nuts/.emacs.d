b01e45c534470874df4c285ba5fe8d5f741fd839
David Johnston <dvdjoh@gmail.com>
2023-12-18 23:45:57 -0500

    Add early-init to make Emacs and Termux talk to eachother

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;; Make emacs on Android work with Termux
+(setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
+		       (getenv "PATH")))
+(setenv "LD_LIBRARY_PATH" (format "%s:%s"
+				  "/data/data/com.termux/files/usr/lib"
+				  (getenv "LD_LIBRARY_PATH")))
+(push "/data/data/com.termux/files/usr/bin" exec-path)
